42bb89fec7ce92390524e1a6da0fac9f4d6bf64d
Ilegubo Kofi Owusu <226963023+Ilegubo@users.noreply.github.com>
2026-06-18 08:09:21 +0100

    Final project, chamber/discover.html responsive image

---
+++ project/scripts/navigation.js
@@ -0,0 +1,7 @@
+const hamButton = document.querySelector("#ham-btn");
+const navLink = document.querySelector("#navigation");
+
+hamButton.addEventListener('click',() =>{
+    hamButton.classList.toggle('show');
+    navLink.classList.toggle('show');
+});
